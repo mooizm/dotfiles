f7073cb6006b20beb5896fa9de99ebc92d2be023
mooizm <amoodeng@gmail.com>
2019-05-02 11:00:30 +1000

    Create dis.sh

---
+++ qemu/dis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+qemu-system-i386 -m 64M -cpu pentium -boot d \
+-hda ../images/dos.img \
+-cdrom ../FD12CD.iso \
+#-drive format=raw,file=../FLOPPY.img,if=floppy,format=raw   \
+-soundhw sb16,adlib,pcspk \
+-vga std \
+-name "Dos" \
+
+# Add floppy
+# -fda ../Windows98.img
