09a16daa48a35f68ea23043b207078ac8035eaee
mooizm <amoodeng@gmail.com>
2019-08-24 08:57:47 +1000

    Update myBash.sh

--- myBash.sh
+++ myBash.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
 set -e
-
 download() {
   URL=https://raw.githubusercontent.com/mooizm/dotfiles/master/.bashrc
   curl --progress-bar -L $URL -o $HOME/myBash
 }
-
 copyover() {
   echo "$(cat $HOME/myBash)" >> $HOME/.bashrc
 }
-
 download
 copyover
